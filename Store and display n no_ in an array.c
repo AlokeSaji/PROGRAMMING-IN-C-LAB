ffb48948b33fb5391914590370e7e7c31502af53
AlokeSaji <96783460+AlokeSaji@users.noreply.github.com>
2022-01-03 21:09:21 +0530

    Add files via upload

---
+++ Store and display n no_ in an array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+  int a[50],i,n;
+  printf("enter the size of array");
+  scanf("%d",&n);
+  printf("enter the array elements");
+  for(i=0;i<n;i++)
+    {
+      scanf("%d",&a[i]);
+    }
+}
